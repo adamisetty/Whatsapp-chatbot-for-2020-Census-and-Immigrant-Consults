9af249629d70b2812774c292ebc46c38613caff2
ankithadamisetty <ankithad@illinois.edu>
2020-06-01 16:53:46 -0500

    Adds whatsapp implementation

---
+++ FBMessengerChatbot/TFDIFwhatsapp.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from FBMessengerChatbot.TFIDF.Transformer import Transformer
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+transformer = Transformer('FBMessengerChatbot/data/train/QnA.csv')
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+
+    if not (incoming_msg == None or incoming_msg == ''):
+        response = transformer.match_query(incoming_msg)
+        msg.body(response)
+        responded = True
+
+    if not responded:
+        msg.body('Sorry, I cannot understand your message')
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run()
+
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from FBMessengerChatbot.TFIDFapp import app
+#from FBMessengerChatbot.TFIDFapp import app
+from FBMessengerChatbot.TFDIFwhatsapp import app
 
 if __name__ == '__main__':
     app.run()
